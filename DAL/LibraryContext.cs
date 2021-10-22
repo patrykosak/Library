3dd6625e11da21b85e65ec471262aaeae7071623
Patryk Osak <pionchaoo@gmail.com>
2021-10-23 00:46:21 +0200

    add dbcontext

---
+++ DAL/LibraryContext.cs
@@ -0,0 +1,25 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Library.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("LibraryContext")
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<PublishingHouse> PublishingHouses { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
