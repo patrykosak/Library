422cb0670e3cc1060bf70f883576b1a1290ca0aa
Patryk Osak <pionchaoo@gmail.com>
2021-12-15 22:48:49 +0100

    add cart items crud

---
+++ Controllers/CartItemController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class CartItemController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: CartItem
+        public ActionResult Index()
+        {
+            var cartItems = db.CartItems.Include(c => c.Book);
+            return View(cartItems.ToList());
+        }
+
+        // GET: CartItem/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // GET: CartItem/Create
+        public ActionResult Create()
+        {
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title");
+            return View();
+        }
+
+        // POST: CartItem/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,CartID,DateCreated,ISBN,Quantity")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CartItems.Add(cartItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title", cartItem.ISBN);
+            return View(cartItem);
+        }
+
+        // GET: CartItem/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title", cartItem.ISBN);
+            return View(cartItem);
+        }
+
+        // POST: CartItem/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,CartID,DateCreated,ISBN,Quantity")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cartItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title", cartItem.ISBN);
+            return View(cartItem);
+        }
+
+        // GET: CartItem/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // POST: CartItem/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CartItem cartItem = db.CartItems.Find(id);
+            db.CartItems.Remove(cartItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
